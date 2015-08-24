38f9eb4e6cab41a2b53c1a5ead378f00d71c719b
Manuel Wiedenmann <manuel@funkensturm.de>
2015-08-24 17:26:53 +0200

    tests

---
+++ tests/dummy/app/models/task.js
@@ -0,0 +1,24 @@
+import DS from 'ember-data';
+
+const {
+  Model,
+  attr,
+  belongsTo,
+  hasMany
+} = DS;
+
+export default Model.extend({
+  name: attr('string'),
+  position: attr('number'),
+
+  parent: belongsTo('task', {
+    async: true,
+    autoSave: true,
+    inverse: 'children'
+  }),
+  children: hasMany('task', {
+    async: true,
+    autoSave: true,
+    inverse: 'parent'
+  })
+});
