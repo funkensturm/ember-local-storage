3ebcc5ca85f5eb0eafce7e182dd078cb83340bc6
Manuel Wiedenmann <manuel@funkensturm.de>
2015-09-03 11:52:31 +0200

    fix ember-beta

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ module.exports = {
     var checker = new VersionChecker(this);
     var dep = checker.for('ember-data', 'bower');
 
-    this.hasEmberData = dep.satisfies('>= 1.13.0');
+    this.hasEmberData = dep.gt('2.0.0') || dep.satisfies('>= 1.13.0');
 
     // determin if saveAs and Blob should be imported
     var projectConfig = this.project.config(app.env);
