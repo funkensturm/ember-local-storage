844ff04fa2ccf2b237732c841c572adaadf72d57
Manuel Wiedenmann <manuel@funkensturm.de>
2015-10-31 15:57:39 +0000

    update ember-cli

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -1,5 +1,7 @@
 window.deprecationWorkflow = window.deprecationWorkflow || {};
 window.deprecationWorkflow.config = {
   workflow: [
+    // ember-data is responsible for that
+    { handler: "silence", matchMessage: "Ember.create is deprecated in favor of Object.create" }
   ]
 };
