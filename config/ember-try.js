0b8a8a3742b6b69330f88135b9d7d599be04bd8b
Ryan Mark <rgmark@gmail.com>
2018-07-31 16:17:51 -0700

    more work

--- config/ember-try.js
+++ config/ember-try.js
@@ -3,42 +3,6 @@ module.exports = {
   useYarn: true,
   scenarios: [
     {
-      name: 'ember-lts-2.4',
-      bower: {
-        dependencies: {
-          'ember': 'components/ember#lts-2-4',
-          'ember-cli-shims': '0.1.0'
-        },
-        resolutions: {
-          'ember': 'lts-2-4'
-        }
-      },
-      npm: {
-        devDependencies: {
-          'ember-data': '~2.4.0',
-          'ember-cli-shims': null,
-          'ember-source': null
-        }
-      }
-    },
-    {
-      name: 'ember-lts-2.8',
-      bower: {
-        dependencies: {
-          'ember': 'components/ember#lts-2-8'
-        },
-        resolutions: {
-          'ember': 'lts-2-8'
-        }
-      },
-      npm: {
-        devDependencies: {
-          'ember-data': '~2.8.0',
-          'ember-source': null
-        }
-      }
-    },
-    {
       name: 'ember-lts-2.12',
       npm: {
         devDependencies: {
