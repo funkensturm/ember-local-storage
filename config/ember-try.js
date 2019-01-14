47cdd67a97715e050881993708f730c8546d9cdc
Manuel Wiedenmann <manuel@funkensturm.de>
2019-01-14 15:25:22 -0500

    fix: Update config/ember-try.js
    Co-Authored-By: knownasilya <knownasilya@gmail.com>

--- config/ember-try.js
+++ config/ember-try.js
@@ -12,6 +12,23 @@ module.exports = function() {
       useYarn: true,
       scenarios: [
         {
+          name: 'ember-lts-2.12',
+          npm: {
+            devDependencies: {
+              'ember-data': '~2.12.0',	
+              'ember-source': '~2.12.0'
+            }
+          }
+        },
+                {
+          name: 'ember-lts-2.16',
+          npm: {
+            devDependencies: {
+              'ember-data': '~2.16.0',	
+              'ember-source': '~2.16.0'
+            }
+          }
+        },
           name: 'ember-lts-2.18',
           env: {
             EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true })
