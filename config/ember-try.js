3454c3ca3d762d71ab1687cd1a8b0ec3a94d408b
Ilya Radchenko <knownasilya@gmail.com>
2019-01-14 12:56:27 -0500

    fix: update travis and ember-try to yarn

--- config/ember-try.js
+++ config/ember-try.js
@@ -9,6 +9,7 @@ module.exports = function() {
     getChannelURL('canary')
   ]).then((urls) => {
     return {
+      useYarn: true,
       scenarios: [
         {
           name: 'ember-lts-2.18',
@@ -54,7 +55,7 @@ module.exports = function() {
             }
           }
         },
-        // The default `.travis.yml` runs this scenario via `npm test`,
+        // The default `.travis.yml` runs this scenario via `yarn test`,
         // not via `ember try`. It's still included here so that running
         // `ember try:each` manually or from a customized CI config will run it
         // along with all the other scenarios.
