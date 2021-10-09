c295eac88f563434b525d6245868939bbe9bfd69
Manuel Wiedenmann <manuel@funkensturm.de>
2021-10-09 20:19:29 +0200

    Update ember-cli to 3.28

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -1,5 +1,21 @@
 'use strict';
 
 module.exports = {
-  extends: 'recommended'
+  extends: 'recommended',
+  rules: {
+    // TODO: Remove over time
+    'link-href-attributes': false,
+    'no-action': false,
+    'no-curly-component-invocation': false,
+    'no-implicit-this': false,
+    'no-inline-styles': false,
+    'no-invalid-interactive': false,
+    'no-link-to-positional-params': false,
+    'no-partial': false,
+    'no-unbalanced-curlies': false,
+    'require-button-type': false,
+    'require-iframe-title': false,
+    'require-input-label': false,
+    'require-valid-alt-text': false,
+  },
 };
--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = function(environment) {
+module.exports = function (environment) {
   let ENV = {
     modulePrefix: 'dummy',
     environment,
@@ -13,8 +13,8 @@ module.exports = function(environment) {
       },
       EXTEND_PROTOTYPES: {
         // Prevent Ember Data from overriding Date.parse.
-        Date: false
-      }
+        Date: false,
+      },
     },
 
     APP: {
@@ -25,7 +25,7 @@ module.exports = function(environment) {
     'ember-local-storage': {
       fileExport: true,
       // ignoreBlobWarning: true,
-    }
+    },
   };
 
   if (environment === 'development') {
