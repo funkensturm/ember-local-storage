65e641822186a108bc0e2ef97fea61d15026b65b
Manuel Wiedenmann <manuel@funkensturm.de>
2021-10-09 16:53:55 +0200

    Add github actions ci workflow

--- config/ember-try.js
+++ config/ember-try.js
@@ -7,53 +7,6 @@ module.exports = async function() {
     useYarn: true,
     scenarios: [
       {
-        name: 'ember-lts-2.12',
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({
-            'jquery-integration': true
-          })
-        },
-        npm: {
-          devDependencies: {
-            '@ember/jquery': '^0.5.1',
-            'ember-cli-shims': '1.1.0',
-            'ember-data': '~2.12.0',
-            'ember-source': '~2.12.0'
-          }
-        }
-      },
-      {
-        name: 'ember-lts-2.13',
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({
-            'jquery-integration': true
-          })
-        },
-        npm: {
-          devDependencies: {
-            '@ember/jquery': '^0.5.1',
-            'ember-cli-shims': '1.1.0',
-            'ember-data': '~2.13.0',
-            'ember-source': '~2.13.0'
-          }
-        }
-      },
-      {
-        name: 'ember-lts-2.16',
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({
-            'jquery-integration': true
-          })
-        },
-        npm: {
-          devDependencies: {
-            '@ember/jquery': '^0.5.1',
-            'ember-data': '~2.16.0',
-            'ember-source': '~2.16.0'
-          }
-        }
-      },
-      {
         name: 'ember-lts-2.18',
         env: {
           EMBER_OPTIONAL_FEATURES: JSON.stringify({
@@ -69,29 +22,38 @@ module.exports = async function() {
         }
       },
       {
-        name: 'ember-lts-3.4',
-        npm: {
-          devDependencies: {
-            'ember-data': '~3.4.0',
-            'ember-source': '~3.4.0'
-          }
-        }
-      },
-      {
-        name: 'ember-lts-3.8',
-        npm: {
-          devDependencies: {
-            'ember-data': '~3.8.0',
-            'ember-source': '~3.8.0'
-          }
-        }
-      },
-      {
         name: 'ember-lts-3.12',
         npm: {
           devDependencies: {
             'ember-data': '~3.12.0',
             'ember-source': '~3.12.0'
+          }
+        }
+      },
+      {
+        name: 'ember-lts-3.16',
+        npm: {
+          devDependencies: {
+            'ember-data': '~3.16.0',
+            'ember-source': '~3.16.0'
+          }
+        }
+      },
+      {
+        name: 'ember-lts-3.20',
+        npm: {
+          devDependencies: {
+            'ember-data': '~3.20.0',
+            'ember-source': '~3.20.0'
+          }
+        }
+      },
+      {
+        name: 'ember-lts-3.24',
+        npm: {
+          devDependencies: {
+            'ember-data': '~3.24.0',
+            'ember-source': '~3.24.0'
           }
         }
       },
