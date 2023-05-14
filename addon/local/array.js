3ec66792be3e5385991e36a76bf65ad652971dc5
Manuel Wiedenmann <manuel@funkensturm.de>
2023-05-14 18:23:39 +0200

    Remove array, object, and storage mixins and helpers/utils

--- addon/local/array.js
+++ addon/local/array.js
@@ -1,6 +1,17 @@
+import { A } from '@ember/array';
 import ArrayProxy from '@ember/array/proxy';
-import ArrayProxyMixin from '../mixins/array';
+import { set } from '@ember/object';
+import StorageProxyMixin from 'ember-local-storage/-private/legacy-mixins/storage';
+import { save } from 'ember-local-storage/-private/legacy-utils';
 
-export default ArrayProxy.extend(ArrayProxyMixin, {
+export default ArrayProxy.extend(StorageProxyMixin, {
+  _initialContent: A(),
   _storageType: 'local',
+
+  _clear() {
+    set(this, 'content', A());
+  },
+
+  replaceContent: save,
+  reset: save,
 });
